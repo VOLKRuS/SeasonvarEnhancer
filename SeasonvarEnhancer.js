6ca3624b46a1db8c50b8eb7eadec50d699758daa
VOLK_RuS <Staggering@yandex.ru>
2021-09-15 04:52:42 +0300

    Create SeasonvarEnhancer.js

---
+++ SeasonvarEnhancer.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Seasonvar enchancer
+// @version      0.1
+// @description  Enhanser for seasonvar.ru
+// @author       VOLK_RuS
+// @match        http://seasonvar.ru/*
+// @updateURL	https://github.com/VOLKRuS/SeasonvarEnhanser/releases/latest/download/SeasonvarEnhanser.js
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+)();
