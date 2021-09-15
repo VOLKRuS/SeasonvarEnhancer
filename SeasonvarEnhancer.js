6d5ea56a6493c6951c00ebd57daadf34cc713c2c
VOLK_RuS <Staggering@yandex.ru>
2021-09-15 05:14:47 +0300

    Update SeasonvarEnhancer.js

--- SeasonvarEnhancer.js
+++ SeasonvarEnhancer.js
@@ -4,7 +4,7 @@
 // @description  Enhanser for seasonvar.ru
 // @author       VOLK_RuS
 // @match        http://seasonvar.ru/*
-// @updateURL	 https://github.com/VOLKRuS/SeasonvarEnhanser/releases/latest/download/SeasonvarEnhancer.js
+// @updateURL	 https://github.com/VOLKRuS/SeasonvarEnhancer/raw/main/SeasonvarEnhancer.js
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
